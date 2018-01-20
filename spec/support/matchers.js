32c901889e42bae70953585060aff880477424e4
Tobias Sandelius <tobias@sandeli.us>
2018-01-20 16:13:24 +0100

    added spec helper

---
+++ spec/support/matchers.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// const expect = require('expect')
+
+// expect.extend({
+//   toBeDivisibleBy(received, argument) {
+//     const pass = received % argument == 0
+//
+//     if (pass) {
+//       return {
+//         message: () =>
+//           `expected ${received} not to be divisible by ${argument}`,
+//         pass: true,
+//       }
+//     } else {
+//       return {
+//         message: () => `expected ${received} to be divisible by ${argument}`,
+//         pass: false,
+//       }
+//     }
+//   },
+// })
+//
+// test('even and odd numbers', () => {
+//   expect(100).toBeDivisibleBy(2)
+//   expect(101).not.toBeDivisibleBy(2)
+// })
